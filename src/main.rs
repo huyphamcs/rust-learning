c072e2cfb637298c66d45af7cafe3d4f14e78c17
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-16 16:41:41 +0700

    Debug:

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
-fn main () {
-    let str = String::from("Hello World");
-    // get_ownership(str);
-    println!("{}", str);
+#[derive(Debug)]
 
+struct Student {
+    name: String,
+    age: usize,
+    email: String,
+    active: bool,
 }
 
-fn get_ownership (str: String) {
-    // println!("{}", str);
+fn main() {
+    let first_student: Student = Student {
+        name: String::from("Pham Quoc Huy"),
+        age: 19,
+        email: String::from("a@gmail.com"),
+        active: false,
+    };
+    let second_student = create_member(
+        first_student.name.clone(),
+        first_student.age,
+        first_student.email.clone(),
+        first_student.active,
+    );
+    println!("{:#?}", first_student);
+
+    println!("{:#?}", second_student);
+}
+
+fn create_member(name: String, age: usize, email: String, active: bool) -> Student {
+    return Student {
+        name,
+        age,
+        email,
+        active,
+    };
 }
