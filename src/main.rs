ec647be14e6b0c040083837762393ca3569a473b
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-16 15:15:11 +0700

    Basic functions example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", add(1,2));
+    println!("Factorial: {}", factorial(2))
+}
+
+
+fn add (a: u64, b: u64)-> u64 {
+    return a+b;
+}
+
+
+
+fn factorial (n: u64) -> u64{
+    if n<=2 {
+        return n;
+    }
+    return n*factorial(n-1);
 }
