824c92be639833ef5cc74bee7d60f0f84018eae4
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-17 08:33:56 +0700

    Some enum

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::collections::HashMap;
 fn main() {
-    let solar_distance = HashMap::from([
-        ("Mercury", 0.4),
-        ("Venus", 0.7),
-        ("Earth", 1.0),
-        ("Mars", 1.5),
-    ]);
-
-    print!("{:?}", solar_distance);
+    let optional = Some(1);
+    match optional {
+        Some(i) => println!("{}", i),
+        None => println!("No value."),
+    }
 }
