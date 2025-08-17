77e44f5455da0a9c03559db69c40fd2eec8f6483
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-17 07:41:47 +0700

    Loops

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,23 @@
-use rand::Rng;
-use std::io; // Import the Rng trait
-fn main() {
-    let random_number = generate_random_number();
-    // let guess = string_to_usize(get_user_input());
-
-    loop {
-        let guess = string_to_usize(get_user_input());
-        if guess > random_number {
-            println!("Lower");
-        }
-        else if guess < random_number {println!("Higher")}
-        else {break;}
+fn main () {
+    print_numbers(10);
+}
+fn print_numbers(n: usize) {
+    
+    // Solution 1
+    // let mut i = 0;
+    // loop {
+    //     println!("{i}");
+    //     i += 1;
+    //     if i == n {break;}
+    // }
+    // Solution 2
+    // let mut i = 0;
+    // while i<n {
+    //     println!("{i}");
+    //     i += 1;
+    // }
+    // Solution 3
+    for i in 0..10 {
+        println!("{i}");
     }
-    println!("Correct");
-
-}
-
-fn generate_random_number() -> usize {
-    let mut rng = rand::thread_rng(); // Get a thread-local random number generator
-
-    // Generate a random number within a specific range (e.g., 0 to 99)
-    let n: usize = rng.gen_range(0..100);
-    // println!("Random number (0-99): {}", n);
-    return n;
-}
-
-fn string_to_usize(str: String) -> usize {
-    return str.parse().expect("Failed to parse");
-}
-
-fn get_user_input() -> String {
-    // println!("Please enter some text:"); // Prompt the user
-
-    let mut input_string = String::new(); // Create a new, mutable String to store the input
-    // let output: String;
-    io::stdin() // Get the standard input handle
-        .read_line(&mut input_string) // Read a line from stdin into the string
-        .expect("Failed to read line"); // Handle potential errors during input
-
-    // println!("You entered: {}", input_string.trim());
-    return String::from(input_string.trim());
 }
