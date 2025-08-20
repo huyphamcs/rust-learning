f79655a3ed3efa9d2a13b679c647eca296848b2c
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-20 15:56:08 +0700

    generic

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-fn main() {
-    let mut s = "first";
-    println!("{}", to_pig_latin(&s))
+struct Container<T> {
+    value: T
 }
 
-fn to_pig_latin(text: &str) -> String {
-    let mut result = String::new();
-    for word in text.split_whitespace() {
-        let mut chars = word.chars();
-        let first_char = match chars.next() {
-            Some(c) => c,
-            None => continue, // Bỏ qua từ rỗng
-        };
-
-        let pig_word = match first_char {
-            'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => {
-                format!("{}-hay", word)
-            }
-            _ => {
-                format!("{}-{}ay", chars.as_str(), first_char)
-            }
-        };
-        result.push_str(&pig_word);
-        result.push(' ');
+impl<T> Container<T> {
+    fn new (value: T) -> Self {
+        Container { value }
     }
-    result.trim().to_string() // Xóa dấu cách thừa ở cuối
+
+    fn get_value(&self) -> &T {
+        &self.value
+    }
+}
+
+fn main () {
+    let cont_1 = Container::new(10);
+    let cont_2 = Container::new("hello");
+    println!("{}", cont_1.get_value());
+    println!("{}", cont_2.get_value());
+
 }
