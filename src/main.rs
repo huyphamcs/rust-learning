fbbd770d7f8448331f2fc6f384a9184c1ff7d3c8
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-20 10:54:03 +0700

    Fibo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ fn main () {
     println!("{}", "Hello World");
     println!("{}", celsius_to_fahrenheit(42.9 as f32));
     println!("{}", fahrenheit_to_celsius(42.9 as f32));
+    println!("{}", fibo(5));
+    println!("{}", fibo(7));
 
 }
 
@@ -11,4 +13,9 @@ fn celsius_to_fahrenheit (celsius: f32) -> f32 {
 
 fn fahrenheit_to_celsius (fahrenheit: f32) -> f32 {
     (fahrenheit - (32 as f32))* (5 as f32) / (9 as f32)
+}
+
+fn fibo (n: usize) -> usize {
+    if n == 1 || n == 2 {n-1}
+    else {fibo(n-1) + fibo(n-2)}
 }
