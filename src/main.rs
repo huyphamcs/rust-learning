49e062f5038464f0eff1330ec3ef2df0cf760169
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-08-20 10:43:36 +0700

    C and F temperature conversion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-fn main() {
-    let optional = Some(1);
-    match optional {
-        Some(i) => println!("{}", i),
-        None => println!("No value."),
-    }
+fn main () {
+    println!("{}", "Hello World");
+    println!("{}", celsius_to_fahrenheit(42.9 as f32));
+    println!("{}", fahrenheit_to_celsius(42.9 as f32));
+
+}
+
+fn celsius_to_fahrenheit (celsius: f32) -> f32 {
+    celsius*(9 as f32) / (5 as f32) + (32 as f32)
+}           
+
+fn fahrenheit_to_celsius (fahrenheit: f32) -> f32 {
+    (fahrenheit - (32 as f32))* (5 as f32) / (9 as f32)
 }
